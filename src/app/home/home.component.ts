fd7d2dcce05d5b3eeade9bf28477b99cb75ace21
ThiaudioTT <thiago.sky25@gmail.com>
2022-11-01 19:10:53 -0300

    WIP: Working on home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,10 +9,15 @@ import { NewsapiService } from '../service/newsapi.service';
 export class HomeComponent implements OnInit {
 
   constructor(private _services:NewsapiService) { }
+  
+  // display data
+  news:any = [];
 
   ngOnInit(): void {
     this._services.homeNews().subscribe((result) => {
       console.log(result);
+      this.news = result;
+      console.log(this.news);
     })
   }
 
