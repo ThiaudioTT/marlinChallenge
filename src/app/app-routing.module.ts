f3decc44e41f5b3e24f20f3d15006d3c26a220ee
ThiaudioTT <thiago.sky25@gmail.com>
2022-10-31 21:42:52 -0300

    first changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+
+// declare a 404 route
+const routes: Routes = [
+  {path: '', component: LayoutComponent,
+    children: [
+      {path: '', component: HomeComponent},
+      {path: 'search', component: SearchComponent},
+    ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
