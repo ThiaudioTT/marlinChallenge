e5fcd9c492399b578927cfe547910b36249c2e62
ThiaudioTT <thiago.sky25@gmail.com>
2022-11-02 11:55:57 -0300

    cleanning...

--- src/app/searchpage/searchpage.component.ts
+++ src/app/searchpage/searchpage.component.ts
@@ -1,14 +1,11 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { NewsapiService } from '../service/newsapi.service'
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom } from 'rxjs'
 
 // algoritmo de busca imutavel
 // Not perfectly, but it works, for now.
-function searchAlgorithm(
-  s: string,
-  data: Array<Object>
-): Array<Object> {
+function searchAlgorithm(s: string, data: Array<Object>): Array<Object> {
   s = s.trim().toLowerCase()
   return data.filter((news: any) => {
     return news['title'] === null
@@ -16,28 +13,6 @@ function searchAlgorithm(
       : news['title'].trim().toLowerCase() === s
   })
 }
-
-/* SECOND
-async function getData(search: string, data: any, _services: NewsapiService) {
-  console.log('getData', data);
-  // get data from service
-  /*
-  _services.getAllNews().subscribe((result) => {
-    console.log("2Rsult: ", result);
-    data = result
-  })
-  
-  await _services.getAllNews().subscribe((result) => {
-    console.log("2Rsult: ", result);
-    data = result
-  })
-
-  // search data
-  data = await searchAlgorithm(search, data);
-  console.log(data)
-  return data;
-}
-*/
 
 @Component({
   selector: 'app-searchpage',
@@ -53,49 +28,15 @@ export class SearchpageComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    /* FIRST
-      this.route.queryParams.subscribe((params) => {
-        this.search = params['s'];
-  
-        
-        // get data from service
-        this._services.getAllNews().subscribe((result) => {
-          this.data = result;
-        });
-
-        // search data
-        this.data = searchAlgorithm(this.search, this.data);
-        console.log(this.data);
-      })
-      */
-     /* SECOND
     this.route.queryParams.subscribe((params) => {
       this.search = params['s']
-
       // get data from service
-      this.data = getData(this.search, this.data, this._services)
-    })*/
-
-    this.route.queryParams.subscribe((params) => {
-      /*
-        if(this.search !== params['s'])
-        {
-          this.search = params['s'];
-          this.ngOnInit();
-
-        }*/
-
-        this.search = params['s'];
-        // get data from service
-        this._services.getAllNews().subscribe((result) => {
-          console.log(result);
-          this.data = result;
-          this.data = searchAlgorithm(this.search, this.data);
-          console.log("Searched: ", this.data);
-        });
-        
+      this._services.getAllNews().subscribe((result) => {
+        console.log(result)
+        this.data = result
+        this.data = searchAlgorithm(this.search, this.data)
+        console.log('Searched: ', this.data)
       })
-
-
+    })
   }
 }
